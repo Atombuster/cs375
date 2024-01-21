72bd8ea2b7ee6f8c7a5b3255c2b859c2f3ea313f
A.K.T <126848536+Atombuster@users.noreply.github.com>
2024-01-21 12:31:25 -0800

    week 2 done

--- week 2/yell.c
+++ week 2/yell.c
@@ -2,6 +2,7 @@
 #include <unistd.h> 
 #include <errno.h> 
 #include <ctype.h>
+#include <stdio.h>// for my testing
 
 #define MAX_BUFFER_SIZE 2048
 //int toupper(int c);
@@ -10,23 +11,26 @@ void perror(const char *s);
 int main(int argc, char *argv[]) {
     char buffer[MAX_BUFFER_SIZE]; 
     ssize_t Read;
-
+	
     if (argc == 1) { //check input file descriptor put it on termanale
         while ((Read = read(0, buffer, MAX_BUFFER_SIZE)) > 0) { 
             write(1, buffer, Read);
         } 
     } else {
-        for (int i = 1; i < argc; i++) { 
+        for (int i = 1; i < argc; i++) {
+		
 		//O_RDONLY opens the file for reading
             int fd = open(argv[i], O_RDONLY); 
-			toupper(fd);
             if (fd == -1) { 
                 perror("Error opening file");
                 continue; 
             }//checked for errors
-
-            while ((Read = read(fd, buffer, MAX_BUFFER_SIZE)) > 0) { 
-                write(1, buffer, Read); 
+		
+		while ((Read = read(fd, buffer, MAX_BUFFER_SIZE)) > 0) { 
+			for (int j = 0; j < Read; j++) {
+                    buffer[j] = toupper(buffer[j]);
+                }// make all letters upppercase
+			write(1, buffer, Read); 
 		
             }//put the file on termanale
 
